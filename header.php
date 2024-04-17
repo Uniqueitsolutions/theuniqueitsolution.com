5dcc690c74da5896679a5dce51ac9ee848a32421
Uniqueitsolutions <149355333+Uniqueitsolutions@users.noreply.github.com>
2024-04-17 17:56:11 +0530

    Update header.php Add Favicon

--- header.php
+++ header.php
@@ -9,8 +9,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <meta name='description' content='Reliable IT Services for Businesses'>
     <!-- Favicon -->
-    <link rel='icon' href='Images/Png_UniqueIT.png'>
-
+<link rel="icon" type="image/x-icon" href="Images/favicon.ico">
     <!-- CSS
         ============================================ -->
 
